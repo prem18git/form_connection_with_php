4358aee3e333940b2bebc174b1f20fe656500abf
prem18git <131329044+prem18git@users.noreply.github.com>
2023-04-21 10:44:32 +0530

    Created a form page for User

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sign Up</title>
+  </head>
+  <body>
+    <h1>Create Your Account</h1>
+    <form action="connection.php" method="post">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name"><br>
+
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email"><br>
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password"><br>
+
+      <input type="submit" value="Sign Up">
+    </form>
+  </body>
+</html>
